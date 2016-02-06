dc08fafebd3f454be07f351ad3ef8aba4d9898e6
Scott Colby <scolby33@gmail.com>
2016-02-06 11:18:50 -0800

    Add flask_marshmallow for object serialization

--- src/wheres_charlie/models.py
+++ src/wheres_charlie/models.py
@@ -1,9 +1,11 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.security import UserMixin, RoleMixin
+from flask_marshmallow import Marshmallow
 
 from . import app
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 roles_users = db.Table('roles_users',
                        db.Column('user_id', db.Integer(), db.ForeignKey('user.user_id')),
@@ -22,11 +24,24 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+    locations = db.relationship('Location', back_populates='user')
 
 
 class Location(db.Model):
     location_id = db.Column(db.Integer(), primary_key=True)
     user_id = db.Column(db.Integer(), db.ForeignKey('user.user_id'))
+    user = db.relationship('User')
     location_string = db.Column(db.String(255))
     date_time = db.Column(db.Date())
     active = db.Column(db.Boolean())
+
+
+class UserSchema(ma.ModelSchema):
+    class Meta:
+        model = User
+
+
+class LocationSchema(ma.ModelSchema):
+    class Meta:
+        model = Location
+
