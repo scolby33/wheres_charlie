47ac89e9fe3968d64b07327d26146b0e3200e303
Scott Colby <scolby33@gmail.com>
2016-02-06 01:56:47 -0800

    Switch to flask-security's user and roles

--- src/wheres_charlie/jwt.py
+++ src/wheres_charlie/jwt.py
@@ -1,41 +1,22 @@
 from flask_jwt import JWT
+from flask_security.utils import verify_and_update_password
 from werkzeug.security import safe_str_cmp
 
-from . import app, handlers
-
-
-class User(object):
-    def __init__(self, user_id, username, password, allowed_scopes):
-        self.id = user_id
-        self.username = username
-        self.password = password
-        self.allowed_scopes = allowed_scopes
-
-    def __str__(self):
-        return "User(id='%s')" % self.id
-
-users = [
-    User(1, 'user1', 'abcxyz', {'test'}),
-    User(2, 'user2', 'abcxyz', {'test'}),
-]
-
-username_table = {u.username: u for u in users}
-user_id_table = {u.id: u for u in users}
-
+from . import app, handlers, models
 
 def authenticate(username, password, scopes):
-    user = username_table.get(username, None)
-    if (user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')) and
-            scopes.issubset(user.allowed_scopes)):
-        return user
+    user = models.user_datastore.get_user(username)
+    if user and verify_and_update_password(password, user):
+        scope_perms = []
+        for scope in scopes:
+            scope_perms.append(user.has_role(scope))
+        if all(scope_perms):
+            return user
 
 
 def identity(payload):
     user_id = payload['sub']
-    return user_id_table.get(user_id, None)
-
-app.app.config['JWT_SECRET_KEY']= 'secret'
-app.app.config['JWT_AUTH_SCOPES_KEY'] = 'scopes'
+    return models.user_datastore.get_user(user_id)
 
 jwt = JWT(app=None, authentication_handler=authenticate, identity_handler=identity)
 jwt.app = app
