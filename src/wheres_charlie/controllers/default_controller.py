ee4cce47550b09debeafbaf5d1bd61f1e376c078
Scott Colby <scolby33@gmail.com>
2016-02-06 00:41:40 -0800

    Stub controller methods

---
+++ src/wheres_charlie/controllers/default_controller.py
@@ -0,0 +1,62 @@
+from ..handlers import jwt_required
+
+
+def locations_get(perPage, page, reverseChronological, showHidden) -> str:
+    return 'do some magic!'
+
+
+def locations_post(body) -> str:
+    return 'do some magic!'
+
+
+@jwt_required()
+def locations_delete() -> str:
+    return 'do some magic!'
+
+
+def locations_id_get(id) -> str:
+    return 'do some magic!'
+
+
+def locations_id_delete(id) -> str:
+    return 'do some magic!'
+
+
+def locations_id_patch(id, body) -> str:
+    return 'do some magic!'
+
+
+def token_post(grantType, username, password, scope) -> str:
+    return 'do some magic!'
+
+
+def users_get(perPage, page, showHidden) -> str:
+    return 'do some magic!'
+
+
+def users_post(body) -> str:
+    return 'do some magic!'
+
+
+def users_delete() -> str:
+    return 'do some magic!'
+
+
+def users_id_get(id) -> str:
+    return 'do some magic!'
+
+
+def users_id_delete(id) -> str:
+    return 'do some magic!'
+
+
+def users_id_patch(id, body) -> str:
+    return 'do some magic!'
+
+
+def users_id_locations_get(id, perPage, page, reverseChronological, showHidden) -> str:
+    return 'do some magic!'
+
+
+def users_id_locations_latest_get(id) -> str:
+    return 'do some magic!'
