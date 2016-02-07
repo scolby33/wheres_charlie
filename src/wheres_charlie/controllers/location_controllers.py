bcd05e1235c9eb5cfd0464572cf90cd0ba1c0332
Scott Colby <scolby33@gmail.com>
2016-02-06 23:56:21 -0800

    Working on the GET /locations method.

--- src/wheres_charlie/controllers/location_controllers.py
+++ src/wheres_charlie/controllers/location_controllers.py
@@ -1,10 +1,28 @@
-from ..handlers import jwt_required
+from flask_jwt import current_identity
+
+from ..handlers import jwt_required, jwt_optional
 from .. import models
 
 
-    query = models.Location.query.order_by(models.Location.date_time).all()
-    return models.LocationSchema().dump(*query).data
+@jwt_optional()
 def locations_get(per_page=10, page=1, reverse_chronological=False, show_hidden=False) -> str:
+    query = models.Location.query
+
+    if not show_hidden or 'admin' not in current_identity.scopes: # TODO fails if current_identity is None
+        query = query.filter_by(active=True)
+
+    if reverse_chronological: # TODO this parameter doesn't seem to be passed correctly...
+        query = query.order_by(models.Location.date_time.desc())
+    else:
+        query = query.order_by(models.Location.date_time.asc())
+
+    query = query.paginate(page, per_page, True)
+
+    rv = []
+    for row in query.items:
+        rv.append(models.LocationSchema().dump(row).data)
+
+    return rv
 
 
 @jwt_required({'admin', 'user:post'})
